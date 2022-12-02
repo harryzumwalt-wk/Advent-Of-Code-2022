cea699ff48c21c0c3967e97942ad0ffb924ac612
harryzumwalt-wk <harry.zumwalt@workiva.com>
2022-12-02 12:15:46 -0600

    cleaner

--- 01/main.js
+++ 01/main.js
@@ -9,19 +9,18 @@ const sumItems = (acc, item) => acc + parseInt(item)
 // data transformations
 const blankLineSplit = (string) => string.split(/\n\s*\n/)
 const newLineSplit = (stringArr) => stringArr.map(string => string.split('\n'))
-const biggestSum = (twoDStringArr) => twoDStringArr.map(x => x.reduce(sumItems, 0)).sort((a, b) => b - a)[0]
-
 const reduceWithSum = (twoDStringArr) => twoDStringArr.map(arr => arr.reduce(sumItems, 0))
-const sortSliceandSum = (numArr) => numArr.sort((a, b) => b - a).slice(0, 3).reduce(sumItems, 0)
+    // 01
+const largestSum = (numArr) => numArr.sort((a, b) => b - a)[0]
+    // 02
+const threeLargestSum = (numArr) => numArr.sort((a, b) => b - a).slice(0, 3).reduce(sumItems, 0)
 
 // composed functions
-const largest = pipe(blankLineSplit, newLineSplit, biggestSum);
-const threeLargestSum = pipe(blankLineSplit, newLineSplit, reduceWithSum, sortSliceandSum)
+const base = pipe(blankLineSplit, newLineSplit, reduceWithSum)
+const largest = pipe(base, largestSum)
+const threeLargest = pipe(base, threeLargestSum)
 
 // main
 const data = fs.readFileSync('./input.txt', {encoding:'utf8', flag:'r'})
-const partOneResult = largest(data)
-const partTwoResult = threeLargestSum(data)
-
-console.log("Part 1 answer: ", partOneResult);
-console.log("Part 2 answer: ", partTwoResult);
+console.log("Part 1 answer: ", largest(data));
+console.log("Part 2 answer: ", threeLargest(data));
